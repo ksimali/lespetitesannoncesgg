7fe5c4a014273253721c17d7384a43af1e9567a7
ksimali <cassim.ali@outlook.fr>
2024-09-09 18:05:02 -0400

    Add header and footer layout in the Homepage(views/index.php) and the category page(views/show.php)

--- views/ad/index.php
+++ views/ad/index.php
@@ -1,1 +1,5 @@
+<?php require VIEW_PATH . '/layouts/header.php'; ?>
+
 <h1> Les petites annoncesGG</h1>
+
+<?php require VIEW_PATH . '/layouts/footer.php' ?>
--- views/category/show.php
+++ views/category/show.php
@@ -1,1 +1,6 @@
+<?php require VIEW_PATH . '/layouts/header.php'; ?>
+
 <h1> les categories d'annonces</h1>
+
+<?php require VIEW_PATH .'layouts/footer.php' ?>
+

e19a784775a46c95985256555e5bc734ad203874
ksimali <cassim.ali@outlook.fr>
2024-09-09 17:36:10 -0400

    Updat index.php in public folder (router file) - refactoring code : - Create VIEW_PATH variable - replace the content in the require route definition

--- public/index.php
+++ public/index.php
@@ -3,14 +3,16 @@ require '../vendor/autoload.php'; // on charge le fichier d'autolading de compos
 
 $router = new AltoRouter(); // On demarre notre router
 
+// definir une partie du chemin dans une variable
+define('VIEW_PATH', dirname(__DIR__) . '/views');
 // map homepage using callable
 $router->map('GET', '/', function(){
-    require dirname(__DIR__) . '/views/ad/index.php';
+    require VIEW_PATH . '/ad/index.php';
 });
 
 // map pages catégories
 $router->map('GET', '/category', function(){
-    require dirname(__DIR__) . '/views/category/show.php';
+    require VIEW_PATH . '/category/show.php';
 });
 
 // demander aux routeur si l'url tapé correspond a une de ces routes.
